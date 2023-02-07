c67259bcb43d5c28e4c9230488961ed139b1f6ce
KellyMark <kmark602@gmail.com>
2023-02-06 19:38:27 -0500

    commit tree 364f5683755608340856606d9e08513dec4b4888

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,8 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  const element = screen.queryByText("please pass this test");
+  screen.debug(element);
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
